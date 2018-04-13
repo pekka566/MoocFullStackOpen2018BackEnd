17935660eb136a565c8ef621c4e5e3f2c5dd6b38
pekka566 <pekka.suomala@gmail.com>
2018-04-13 21:52:37 +0300

    Initiate app.

--- osa3/persons/index.js
+++ osa3/persons/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 //app.use(morgan("tiny"))
 app.use(morgan(':method :url :req :status :res[content-length] - :response-time ms'))
@@ -71,7 +73,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
